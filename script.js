6326737f66b8ce89a143bb395eeecd8e934aee7d
xoaic <xoaic@xcc.one>
2023-02-20 17:03:50 +0700

    update project star-blue 1.4

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const sideBar = document.querySelector("#side-bar");
 const content = document.querySelector("#content");
 const webPagItems = webPag.querySelectorAll("a");
 const sections = document.querySelectorAll(".section");
+const menuBtn = document.querySelector("#menu-btn");
 
 // async function updateBodyBackground() {
 //     fetch('https://api.bing.com/images/search?q=daily+image', { mode: 'no-cors' })
@@ -33,12 +34,12 @@ function disableCopy() {
         e.preventDefault();
         e.stopPropagation();
     }
-    document.onkeydown = function(e) {
-        if (e.keyCode === 123) {
-            e.preventDefault();
-            return false;
-        }
-    };
+    // document.onkeydown = function(e) {
+    //     if (e.keyCode === 123) {
+    //         e.preventDefault();
+    //         return false;
+    //     }
+    // };
     document.body.style.userSelect = "none";
     document.body.style.msUserSelect = "none";
     document.body.style.mozUserSelect = "none";
@@ -51,56 +52,40 @@ function toggleButton() {
             webPagItems.forEach((item) => {
                 item.classList.remove("active-pag");
             });
-            item.classList.add("active-pag")
+            item.classList.add("active-pag");
         })
     })
 
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
     const navBar = document.querySelector(".nav-bar");
     const navBarItems = navBar.querySelectorAll("a");
+
+    if (isMobile) {
+        menuBtn.classList.remove("hidden");
+        sideBar.classList.add("hidden");
+    }
+
+    menuBtn.addEventListener("click", () => {
+        menuBtn.classList.add("hidden");
+        sideBar.classList.remove("hidden");
+    })
+
+    sideBar.addEventListener("click", () => {
+        if (isMobile) {
+            menuBtn.classList.remove("hidden");
+            sideBar.classList.add("hidden");
+        }
+    })
 
     navBarItems.forEach((item) => {
         item.addEventListener("click", () => {
             navBarItems.forEach((item) => {
                 item.classList.remove("active-nav");
             });
-            item.classList.add("active-nav")
+            item.classList.add("active-nav");
         })
     })
 }
 
-function toggleSideBar() {
-    let timeoutId;
-
-    content.addEventListener("scroll", () => {
-        sideBar.classList.remove("side-bar-hidden");
-        sideBar.classList.add("side-bar-show");
-        content.classList.remove("content-full");
-        content.classList.add("content-part");
-
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(() => {
-            sideBar.classList.remove("side-bar-show");
-            sideBar.classList.add("side-bar-hidden");
-            content.classList.remove("content-part");
-            content.classList.add("content-full");
-        }, 3000);
-
-        // const currentPosition = content.scrollY + (content.innerHeight / 2);
-        // sections.forEach(section => {
-        //     const sectionTop = section.offsetTop;
-        //     const sectionBottom = section.offsetTop + section.offsetHeight;
-        //     if (currentPosition >= sectionTop && currentPosition < sectionBottom) {
-        //         navBarItems.forEach(item => {
-        //             item.classList.remove("active-nav");
-        //             if (item.getAttribute("href") === `#${section.id}`) {
-        //                 item.classList.add("active-nav");
-        //             }
-        //         });
-        //     }
-        // });
-    });
-}
-
 disableCopy();
 toggleButton();
-toggleSideBar();
